49728643fd0e96e3e4abe5910033ccab2fcd9e0b
Adrian Szlegel <adrian.szlegel@hotmail.com>
2022-09-28 21:55:22 +0200

    added scroll to top icon & grid skeleton added & header created with starter styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import {AppBar, Toolbar, Typography, Container, Fab} from '@mui/material';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import ScrollTop from './utils/ScrollTop';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AppBar className="main-header">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            React Photo Gallery
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <Container>
+        {/* main content here */}
+        <h4>Title</h4>
+        <div style={{height: "2000px", backgroundColor: "green"}}></div>
+        {/* main content here */}
+      </Container>
+      <ScrollTop>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
   );
 }
 
---
+++ src/utils/ScrollTop.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useScrollTrigger, Fade, Box} from '@mui/material';
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger();
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
+export default ScrollTop;
