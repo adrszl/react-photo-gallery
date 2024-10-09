c35355c3c33e35391c1aacdebb9c5743bb5d1834
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-10-09 22:38:51 +0200

    added grid and few features' skeletons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import {AppBar, Toolbar, Typography, Container, Fab} from '@mui/material';
+import React, {useState} from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Fab,
+  Grid,
+  Paper,
+  Select,
+  MenuItem,
+  ImageList,
+  ImageListItem,
+  ListSubheader,
+  ImageListItemBar,
+  IconButton
+} from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import ScrollTop from './utils/ScrollTop';
 import './App.css';
 
 function App() {
+  const [gridStyle, setGridStyle] = useState('standard');
+
+  const handleGridStyleChange = (event) => {
+    setGridStyle(event.target.value);
+  }
+
   return (
     <React.Fragment>
       <AppBar className="main-header">
@@ -18,7 +40,84 @@ function App() {
       <Container>
         {/* main content here */}
         <h4>Title</h4>
-        <div style={{height: "2000px", backgroundColor: "green"}}></div>
+        <p>TBD:</p>
+        <ul>
+          <li>choose light or dark theme</li>
+        </ul>
+        <Select
+          labelId="choose-grid-style"
+          id="choose-grid-style"
+          value={gridStyle}
+          label="Grid style"
+          onChange={handleGridStyleChange}
+        >
+          <MenuItem value={'standard'}>Standard</MenuItem>
+          <MenuItem value={'masonry'}>Masonry</MenuItem>
+        </Select>
+        <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+          <ImageList>
+            <ImageListItem key="Subheader" cols={2}>
+              <ListSubheader component="div">Gallery</ListSubheader>
+            </ImageListItem>
+            <ImageListItem key={'item.img'}>
+              <Grid item>
+                <Paper>
+                  <div style={{height: "200px", width: "200px", backgroundColor: "green"}}></div>
+                  <ImageListItemBar
+                    title={'item.title'}
+                    subtitle={'item.author'}
+                    actionIcon={
+                    <IconButton
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about (...)`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+                </Paper>
+              </Grid>
+            </ImageListItem>
+            <ImageListItem key={'item.img'}>
+              <Grid item>
+                <Paper>
+                  <div style={{height: "200px", width: "200px", backgroundColor: "green"}}></div>
+                  <ImageListItemBar
+                    title={'item.title'}
+                    subtitle={'item.author'}
+                    actionIcon={
+                    <IconButton
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about (...)`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                      }
+                    />
+                </Paper>
+              </Grid>
+            </ImageListItem>
+            <ImageListItem key={'item.img'}>
+              <Grid item>
+                <Paper>
+                  <div style={{height: "200px", width: "200px", backgroundColor: "green"}}></div>
+                  <ImageListItemBar
+                    title={'item.title'}
+                    subtitle={'item.author'}
+                    actionIcon={
+                    <IconButton
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about (...)`}
+                    >
+                          <InfoIcon />
+                        </IconButton>
+                      }
+                    />
+                </Paper>
+              </Grid>
+            </ImageListItem>
+          </ImageList>
+        </Grid>
         {/* main content here */}
       </Container>
       <ScrollTop>
